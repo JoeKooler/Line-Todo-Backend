06dbe4128f265c6834213488858f9cc5f97947e3
JoeKooler <jojesin@gmail.com>
2020-10-16 19:02:38 +0700

    Properly return async val

--- MiddleWares/Auth.js
+++ MiddleWares/Auth.js
@@ -4,30 +4,24 @@ const User = require("../Models/User");
 const channel_id = process.env.LINE_CHANNEL_ID;
 
 const UserVerify = async (access_token) => {
-  let resData = "";
-  await axios
+  const resData = await axios
     .get(
       `https://api.line.me/oauth2/v2.1/verify?access_token=${access_token}`,
       { json: true }
     )
-    .then((res) => {
-      console.log(res.data);
-      resData = res.data;
-    });
+    .then((res) => res.data);
   return resData === channel_id;
 };
 const GetUserID = async (access_token) => {
-  let user = "";
-  await axios({
+  const userID = await axios({
     method: "get",
     url: "https://api.line.me/v2/profile",
     headers: { Authorization: `Bearer ${access_token}` },
     json: true,
   }).then((res) => {
-    console.log(res.data);
-    user = res.data;
+    res.data.userId;
   });
-  return user.userId;
+  return userID;
 };
 
 const Auth = async (req, res, next) => {
